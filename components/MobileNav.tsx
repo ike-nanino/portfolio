bcb5f884ca8f0a676a3b340022faef1dd4b659b7
isaacnanino <isaacnanino@gmail.com>
2024-10-13 06:30:08 -0300

    mobile nav fix

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation"
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { CiMenuFries } from 'react-icons/ci'
 import Link from "next/link";
+import { useState } from "react";
 
 
 
@@ -32,11 +33,22 @@ const links = [
 ]
 
 function MobileNav() {
+
+     const [isOpen, setIsOpen] = useState(false);
     const pathname = usePathname();
+
+    const handleLinkClick = () => {
+        setIsOpen(false);
+      };
   return (
-    <Sheet>
-        <SheetTrigger className="flex justify-center items-center">
-            <CiMenuFries className="text-[32px] text-accent"/>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger className="flex justify-center items-center" asChild>
+        <button
+          className="flex justify-center items-center"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <CiMenuFries className="text-[32px] text-accent" />
+        </button>
         </SheetTrigger>
         <SheetContent side='left' className='flex flex-col'>
             <div className='mt-24 mb-28 text-center text-2xl'> Isaac </div>
@@ -48,6 +60,7 @@ function MobileNav() {
                         href={link.path}
                         key={index}
                         className={`${link.path === pathname && 'text-accent'} text-xl capitalize hover:text-accent ease-in duration-500`}
+                        onClick={handleLinkClick}
                         >
                         {link.name}
                         </Link>
